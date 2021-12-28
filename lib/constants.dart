2cff8efe0a805e32c0f170f86ee92d2002f1139a
Cotliner <carthymariejoseph@gmail.com>
2021-12-28 16:36:20 +0100

    Update code

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFF2697FF);
+const primaryColor = Color(0xFF1A5CFF);
 const secondaryColor = Color(0xFF2A2D3E);
 const bgColor = Color(0xFF212332);
+
+const success = Color(0xFF46C93A);
+const danger = Color(0xFFFF4757);
+const warn = Color(0xFFFFBA02);
 
 const defaultPadding = 16.0;
