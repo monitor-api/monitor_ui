14668f7aef20ef08349c0c3242b83e0544b5f9d2
Omar Makhlas <omar.makhlas@europcar.com>
2022-01-03 14:29:55 +0100

    add DataGrid layer

--- lib/components/dev2/dev2_dashboard.dart
+++ lib/components/dev2/dev2_dashboard.dart
@@ -1,21 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:monitor_ui/Utils.dart';
 import 'package:monitor_ui/components/environment/environment_status.dart';
 import 'package:monitor_ui/constants.dart';
+import 'package:monitor_ui/data/employee.dart';
 import 'package:monitor_ui/pages/header.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
-class Dev2Dashboard extends StatelessWidget {
+class Dev2Dashboard extends StatefulWidget {
   const Dev2Dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<Dev2Dashboard> createState() => _Dev2DashboardState();
+
+}
+
+class _Dev2DashboardState extends State<Dev2Dashboard> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  updateWidget() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: SingleChildScrollView(
-        padding: const EdgeInsets.all(defaultPadding),
+        padding: const EdgeInsets.all(defaultPadding * 2),
         child: Column(
-          children: const [
-            Header(),
-            SizedBox(height: defaultPadding * 2),
-            StatusByEnvironment()
+          children: [
+            const Header(),
+            const SizedBox(height: defaultPadding),
+            const StatusByEnvironment(),
+            const SizedBox(height: defaultPadding),
+            Container(
+              margin: const EdgeInsets.only(left: defaultPadding),
+              padding: const EdgeInsets.all(defaultPadding),
+              decoration: boxDecorationWithBorder(color: secondaryColor, radius: 7),
+              child: Column(
+                children: [
+                  SfDataGrid(
+                    source: employeeDataSource(),
+                    columnWidthMode: ColumnWidthMode.fill,
+                    columns: <GridColumn>[
+                      GridColumn(
+                          columnName: 'id',
+                          label: Container(
+                              padding: const EdgeInsets.all(16.0),
+                              alignment: Alignment.center,
+                              child: const Text('ID', overflow: TextOverflow.ellipsis)
+                          )
+                      ),
+                      GridColumn(
+                          columnName: 'name',
+                          label: Container(
+                              padding: const EdgeInsets.all(8.0),
+                              alignment: Alignment.center,
+                              child: const Text('Name', overflow: TextOverflow.ellipsis)
+                          )
+                      ),
+                      GridColumn(
+                          columnName: 'designation',
+                          label: Container(
+                              padding: const EdgeInsets.all(8.0),
+                              alignment: Alignment.center,
+                              child: const Text(
+                                'Designation',
+                                overflow: TextOverflow.ellipsis,
+                              )
+                          )
+                      ),
+                      GridColumn(
+                          columnName: 'salary',
+                          label: Container(
+                              padding: const EdgeInsets.all(8.0),
+                              alignment: Alignment.center,
+                              child: const Text('Salary', overflow: TextOverflow.ellipsis)
+                          )
+                      ),
+                    ],
+
+                  ),
+                  Container(
+                    //margin: EdgeInsets.only(left: 50, right: 50),
+                    padding: EdgeInsets.only(left: 150, right: 150),
+                    height: 60,
+                    //color: secondaryColor,
+                    child: SfDataPager(
+                      pageCount: (getEmployeeData().length / 10).ceilToDouble(),
+                      delegate: employeeDataSource(),
+                      direction: Axis.horizontal,
+                    ),
+                  )
+                ],
+              )
+            ),
           ],
         ),
       )
---
+++ lib/data/employee.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+
+/// Custom business object class which contains properties to hold the detailed
+/// information about the employee which will be rendered in datagrid.
+class Employee {
+  /// Creates the employee class with required details.
+  Employee(this.id, this.name, this.designation, this.salary);
+
+  /// Id of an employee.
+  final int id;
+
+  /// Name of an employee.
+  final String name;
+
+  /// Designation of an employee.
+  final String designation;
+
+  /// Salary of an employee.
+  final int salary;
+}
+
+/// An object to set the employee collection data source to the datagrid. This
+/// is used to map the employee data to the datagrid widget.
+class EmployeeDataSource extends DataGridSource {
+  /// Creates the employee data source class with required details.
+  EmployeeDataSource({required List<Employee> employeeData}) {
+    _employeeData = employeeData
+        .map<DataGridRow>((e) => DataGridRow(cells: [
+      DataGridCell<int>(columnName: 'id', value: e.id),
+      DataGridCell<String>(columnName: 'name', value: e.name),
+      DataGridCell<String>(columnName: 'designation', value: e.designation),
+      DataGridCell<int>(columnName: 'salary', value: e.salary),
+    ])).toList(growable: false);
+  }
+
+  List<DataGridRow> _employeeData = [];
+  List<Employee> paginatedDataSource = [];
+  final int rowsPerPage = 10;
+
+  @override
+  List<DataGridRow> get rows => _employeeData;
+
+  @override
+  DataGridRowAdapter buildRow(DataGridRow row) {
+    return DataGridRowAdapter(
+        cells: row.getCells().map<Widget>((e) {
+          return Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(8.0),
+            child: Text(e.value.toString()),
+          );
+        }).toList());
+  }
+}
+
+List<Employee> getEmployeeData() {
+  return [
+    Employee(10001, 'James', 'Project Lead', 20000),
+    Employee(10002, 'Kathryn', 'Manager', 30000),
+    Employee(10003, 'Lara', 'Developer', 15000),
+    Employee(10004, 'Michael', 'Designer', 15000),
+    Employee(10005, 'Martin', 'Developer', 15000),
+    Employee(10006, 'Newberry', 'Developer', 15000),
+    Employee(10007, 'Balnc', 'Developer', 15000),
+    Employee(10008, 'Perry', 'Developer', 15000),
+    Employee(10009, 'Gable', 'Developer', 15000),
+    Employee(10010, 'Grimes', 'Developer', 15000)
+  ];
+}
+EmployeeDataSource employeeDataSource() => EmployeeDataSource(employeeData: getEmployeeData());
